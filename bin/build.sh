aaf16819307c0440bab3a0663cee9728e1bcdc2c
Frank Broda <fbroda@ipb-halle.de>
2022-01-13 17:50:24 +0100

    Avoid pulling unneeded images

--- bin/build.sh
+++ bin/build.sh
@@ -14,9 +14,9 @@ cleanup() {
 trap "cleanup; exit 1" ERR
 
 cd "$DIR" 
- grep --include Dockerfile -rhE "^FROM " . | \
-   cut -d' ' -f2- | tr -d ' ' | sort | uniq | \
-   xargs -l1 docker pull 
+# grep --include Dockerfile -rhE "^FROM " . | \
+#   cut -d' ' -f2- | tr -d ' ' | sort | uniq | \
+#   xargs -l1 docker pull 
 
 cd "$DIR/crimsyplugins" 
 docker build -t ipbhalle/crimsyplugins .
