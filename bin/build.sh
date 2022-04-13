0a8f049cffd4673df517cecb7e9de948956cd7b4
Frank Broda <fbroda@ipb-halle.de>
2022-04-13 16:55:26 +0200

    improve build script, disable schedule

--- bin/build.sh
+++ bin/build.sh
@@ -13,15 +13,10 @@ cleanup() {
 
 trap "cleanup; exit 1" ERR
 
-cd "$DIR" 
-# grep --include Dockerfile -rhE "^FROM " . | \
-#   cut -d' ' -f2- | tr -d ' ' | sort | uniq | \
-#   xargs -l1 docker pull 
-
 cd "$DIR/crimsyplugins" 
-docker build -t ipbhalle/crimsyplugins .
+docker build --pull -t ipbhalle/crimsyplugins .
 docker push ipbhalle/crimsyplugins
 
 cd "$DIR/crimsydb/bingo_pg12" 
-docker build -t ipbhalle/crimsydb:bingo_pg12 .
+docker build --pull -t ipbhalle/crimsydb:bingo_pg12 .
 docker push ipbhalle/crimsydb:bingo_pg12
